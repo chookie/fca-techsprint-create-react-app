64043d500bf299b5c93236578c7baf4072a4cf4d
chookie <a.johnston@cardano.com>
2018-11-28 11:31:51 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, {Component} from "react";
+import Navigation from './components/Navigation';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Navigation/>
+      </div>
+    );
+  }
+}
+
+export default App;
